5344bed893e353073a12e5e19912bf878136e42e
Konstantin Ignatiev <kostya@inet-lab.net>
2021-12-31 09:46:44 -0500

    Multi-touch pan & zoom

--- src/life-display.js
+++ src/life-display.js
@@ -29,6 +29,118 @@ Canvas.prototype.strokeRect = function(x, y, w, h) {
   this.ctx.strokeRect(x*this.scale.x, y*this.scale.y, w*this.scale.x, h*this.scale.y);
 }
 
+function ShiftZoom(elm, cvs, update_fn) {
+  this.elm = elm;
+  this.cvs = cvs;
+  this.vp = {};
+  this.update_fn = update_fn;
+  this.touches = [];
+  this.ts = 0;
+
+  this.old_x = null;
+  this.old_y = null;
+}
+
+ShiftZoom.prototype.scroll = function (dx, dy) {
+  this.cvs.vp.x0 += dx;
+  this.cvs.vp.y0 += dy;
+
+  this._redraw(this.cvs.vp);
+}
+
+ShiftZoom.prototype.zoom = function (k, x, y) {
+  const dx = x - this.cvs.vp.x0;
+  const dy = y - this.cvs.vp.y0;
+
+  this.cvs.vp.cell *= k;
+  this.cvs.vp.x0 = x - dx/k;
+  this.cvs.vp.y0 = y - dy/k;
+
+  this._redraw(this.cvs.vp);
+}
+
+ShiftZoom.prototype.down = function (id, x, y) {
+  const idx = this.touches.findIndex(t => t.id === id);
+  if (idx >= 0) return;
+  if (this.touches.length >= 2)
+    return;
+
+  if (this.touches.length === 1) {
+    this.cvs.vp = {...this.vp};
+    const t = this.touches[0];
+    t.x0 = t.x;
+    t.y0 = t.y;
+  }
+
+  this.elm.setPointerCapture(id);
+  this.touches.push({id: id, x0: x, y0: y, x: x, y : y});
+  this.vp = {...this.cvs.vp};
+}
+
+ShiftZoom.prototype._adjust_vp = function(a, b) {
+  if (b) {
+    const old_d = (a.x0 - b.x0)**2 + (a.y0 - b.y0)**2;
+    const new_d = (a.x - b.x)**2 + (a.y - b.y)**2;
+    const os = this.cvs.vp.cell;
+    const ns = os * new_d/old_d;
+    const shift = {x: ((a.x + b.x)/ns - (a.x0 + b.x0)/os)/2, y: ((a.y + b.y)/ns - (a.y0 + b.y0)/os)/2};
+
+    this.vp.cell = ns;
+    this.vp.x0 = this.cvs.vp.x0 - shift.x;
+    this.vp.y0 = this.cvs.vp.y0 - shift.y;
+  }
+  else {
+    this.vp.cell = this.cvs.vp.cell;
+    this.vp.x0 = this.cvs.vp.x0 - (a.x - a.x0) / this.vp.cell;
+    this.vp.y0 = this.cvs.vp.y0 - (a.y - a.y0) / this.vp.cell;
+  }
+}
+
+ShiftZoom.prototype.move = function (id, x, y) {
+  const idx = this.touches.findIndex(x => x.id === id);
+  if (idx < 0) return;
+
+  this.touches[idx].x = x;
+  this.touches[idx].y = y;
+
+  this._adjust_vp(...this.touches);
+  this._redraw(this.vp);
+}
+
+ShiftZoom.prototype.up = function (id) {
+  const idx = this.touches.findIndex(x => x.id === id);
+  if (idx < 0) return;
+
+  this.touches.splice(idx, 1);
+  this.elm.releasePointerCapture(id);
+  this.cvs.vp = {...this.vp};
+
+  if (this.touches.length === 1) {
+    const t = this.touches[0];
+    t.x0 = t.x;
+    t.y0 = t.y;
+  }
+}
+
+ShiftZoom.prototype._redraw = function (vp) {
+  if (this.old_x && this.old_y) {
+    const d = {x: Math.abs(vp.x0 - this.old_x), y: Math.abs(vp.y0 - this.old_y)};
+    if (d.x > 2)
+      console.log("big difference X =", d.x);
+    if (d.y > 2)
+      console.log("big difference Y =", d.y);
+  }
+  this.old_x = vp.x0;
+  this.old_y = vp.y0;
+  const frozen_vp = {...vp};
+  window.requestAnimationFrame(ts => {
+    if (ts > this.ts) {
+      this.ts = ts;
+      this.update_fn(this.cvs, frozen_vp);
+    }
+  });
+}
+
 function init (elm_map, elm_ovw, life_api, linear_memory) {
   const env = get_envelope(life_api, linear_memory);
   console.log("envelope =", env);
@@ -45,7 +157,7 @@ function init (elm_map, elm_ovw, life_api, linear_memory) {
   console.log("MAP:", map.W, map.H, elm_map.width, elm_map.height);
   console.log("OVW:", ovw.W, ovw.H, elm_ovw.width, elm_ovw.height);
 
-  update_map (life_api, linear_memory, map, ovw, env);
+  update_map (life_api, linear_memory, map, map.vp, ovw, env);
 
   // credit: https://codepen.io/AbramPlus/pen/mdymKom
   let ready_to_drag = false;
@@ -67,7 +179,7 @@ function init (elm_map, elm_ovw, life_api, linear_memory) {
     map.vp.x0 = vp_start.x - move.x / map.vp.cell;
     map.vp.y0 = vp_start.y - move.y / map.vp.cell;
 
-    update_map (life_api, linear_memory, map, ovw, env);
+    update_map (life_api, linear_memory, map, map.vp, ovw, env);
   };
 
   const scaleCanvasTouch = () => {
@@ -156,40 +268,29 @@ function init (elm_map, elm_ovw, life_api, linear_memory) {
     elm_map.addEventListener(x, dragZoom));
 */
 
-  let drag_start = null;
+  const shiftZoom = new ShiftZoom(elm_map, map,
+        (cvs, vp) => update_map (life_api, linear_memory, cvs, vp, ovw, env));
+
   const pointerDragZoom = evt => {
     evt.preventDefault();
-    if (evt.type === "pointerdown") {
-      drag_start = {x: evt.pageX, y: evt.pageY};
-      vp_start = {...map.vp};
-      elm_map.setPointerCapture(evt.pointerId);
-    }
-    else if (evt.type === "pointermove") {
-      if (drag_start !== null) {
+    const rect = elm_map.getBoundingClientRect();
 
-        map.vp.x0 = vp_start.x0 - (evt.pageX - drag_start.x) / map.vp.cell;
-        map.vp.y0 = vp_start.y0 - (evt.pageY - drag_start.y) / map.vp.cell;
-
-        redraw = window.requestAnimationFrame(() => update_map (life_api, linear_memory, map, ovw, env));
-      }
-    }
-    else {
-      drag_start = null;
-      elm_map.releasePointerCapture(evt.pointerId);
-      window.cancelAnimationFrame(redraw);
-    }
+    if (evt.type === "pointerdown")
+      shiftZoom.down(evt.pointerId, evt.clientX - rect.left, evt.clientY - rect.top);
+    else if (evt.type === "pointermove")
+      shiftZoom.move(evt.pointerId, evt.clientX - rect.left, evt.clientY - rect.top);
+    else
+      shiftZoom.up(evt.pointerId);
   };
 
-  const wheelZoom = evt => {
+  const wheelZoom_TBR = evt => {
     // https://kenneth.io/post/detecting-multi-touch-trackpad-gestures-in-javascript
     evt.preventDefault();
     if (evt.ctrlKey) {
       const rect = elm_map.getBoundingClientRect();
-      const map_top = rect.top + window.scrollY;
-      const map_left = rect.left + window.scrollX;
 
-      const dx = (evt.clientX - map_left)/map.vp.cell;
-      const dy = (evt.clientY - map_top)/map.vp.cell;
+      const dx = (evt.clientX - rect.left)/map.vp.cell;
+      const dy = (evt.clientY - rect.top)/map.vp.cell;
       const x = map.vp.x0 + dx;
       const y = map.vp.y0 + dy;
 
@@ -198,14 +299,32 @@ function init (elm_map, elm_ovw, life_api, linear_memory) {
       map.vp.x0 = x - dx/k;
       map.vp.y0 = y - dy/k;
 
-      update_map (life_api, linear_memory, map, ovw, env);
+      update_map (life_api, linear_memory, map, map.vp, ovw, env);
     }
     else {
       // With with type of drag, mouse pointer stays still; not sure why there is coefficient "2"
       map.vp.x0 += 2 * evt.deltaX / map.vp.cell;
       map.vp.y0 += 2 * evt.deltaY / map.vp.cell;
 
-      update_map (life_api, linear_memory, map, ovw, env);
+      update_map (life_api, linear_memory, map, map.vp, ovw, env);
+    }
+  }
+
+  const wheelZoom = evt => {
+    evt.preventDefault();
+
+    if (evt.ctrlKey) {
+      const rect = elm_map.getBoundingClientRect();
+      const x = map.vp.x0 + (evt.clientX - rect.left)/map.vp.cell;
+      const y = map.vp.y0 + (evt.clientY - rect.top)/map.vp.cell;
+
+      shiftZoom.zoom(1 - evt.deltaY * 0.01, x, y);
+    }
+    else {
+      const dx = 2 * evt.deltaX / map.vp.cell;
+      const dy = 2 * evt.deltaY / map.vp.cell;
+
+      shiftZoom.scroll(dx, dy);
     }
   }
 
@@ -222,7 +341,6 @@ function get_envelope(life_api, linear_memory) {
 }
 
 function update_ovw(ovw, env, win) {
-
   ovw.ctx.fillStyle = COLORS.ovw.bg;
   ovw.fillRect(0, 0, ovw.W, ovw.H);
 
@@ -244,17 +362,19 @@ function update_ovw(ovw, env, win) {
   ovw.strokeRect((win.x0 - ext.x0)*scale, (win.y0 - ext.y0)*scale, (win.x1 - win.x0)*scale, (win.y1 - win.y0)*scale);
 }
 
-function update_map (life_api, linear_memory, map, ovw, env) {
-  const win = {x0: map.vp.x0, x1: map.vp.x0 + map.W/map.vp.cell, y0: map.vp.y0, y1: map.vp.y0 + map.H/map.vp.cell};
+function update_map (life_api, linear_memory, map, vp, ovw, env) {
+  const win = {x0: vp.x0, x1: vp.x0 + map.W/vp.cell, y0: vp.y0, y1: vp.y0 + map.H/vp.cell};
 
   update_ovw(ovw, env, win);
 
-  // console.log("Region: ", map.vp.x0, x1, map.vp.y0, y1);
+  // console.log("Region: ", vp.x0, x1, vp.y0, y1);
   const [ix0, ix1, iy0, iy1] = [Math.max(env.x0, Math.floor(win.x0)), Math.min(env.x1, Math.ceil(win.x1)),
                                 Math.max(env.y0, Math.floor(win.y0)), Math.min(env.y1, Math.ceil(win.y1))];
 
   // console.log("Integer region:", ix0, ix1, iy0, iy1);
   const [X, Y] = [ix1 - ix0 + 1, iy1 - iy0 + 1];
+  if (X <= 0 || Y <= 0) return;
+
   const nCols = Math.min(X, Math.floor(RESERVED_REGION/Y));
   assert(nCols > 0);
 
@@ -274,9 +394,9 @@ function update_map (life_api, linear_memory, map, ovw, env) {
     for (let y = 0; y < Y; y ++)
       for (let x = 0; x < Xb; x ++)
         if (1 === linear_memory[region + y*Xb + x]) {
-          const xs = (ix0 + xb0 + x - map.vp.x0) * map.vp.cell;
-          const ys = (iy0 +       y - map.vp.y0) * map.vp.cell;
-          map.ctx.fillRect(xs * map.scale.x, ys * map.scale.y, map.vp.cell * map.scale.x,map.vp.cell * map.scale.y);
+          const xs = (ix0 + xb0 + x - vp.x0) * vp.cell;
+          const ys = (iy0 +       y - vp.y0) * vp.cell;
+          map.ctx.fillRect(xs * map.scale.x, ys * map.scale.y, vp.cell * map.scale.x,vp.cell * map.scale.y);
         }
   }
 }
